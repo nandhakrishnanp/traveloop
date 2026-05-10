781c0e97f1f072a2eee5d3e75af0e6339cf8badb
nandhakrishnanp <nandhakrishnandev@gmail.com>
2026-05-10 11:47:23 +0530

    fix: update axios base URL to correct endpoint

--- frontend/src/app/axiosconfig.js
+++ frontend/src/app/axiosconfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: "https://effective-space-waddle-7wprrgw7x6q3rw9v-8080.app.github.dev/v1",
+    baseURL: "https://super-duper-enigma-xxv99jxr44wfpqqp-8080.app.github.dev/v1",
     headers: {
         "Content-Type": "application/json",
     },
