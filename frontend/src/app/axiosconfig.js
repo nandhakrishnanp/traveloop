d54be3616bdcf5be1b3a9fc6a468764399c32032
nandhakrishnanp <nandhakrishnandev@gmail.com>
2026-05-10 12:16:48 +0530

    refactor: reorganize axios instance configuration and request interceptor

--- frontend/src/app/axiosconfig.js
+++ frontend/src/app/axiosconfig.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
-    baseURL: "https://super-duper-enigma-xxv99jxr44wfpqqp-8080.app.github.dev/v1",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "https://super-duper-enigma-xxv99jxr44wfpqqp-8080.app.github.dev/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+// Request interceptor to add JWT token
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
